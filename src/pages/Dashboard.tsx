8b582fcd2a1592c24c6f6fd1be994f27c6439de5
Rian Ahmad <musekryan@gmail.com>
2026-02-28 02:55:05 +0700

    persist

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@ import { useAuth } from "../hooks/useAuth";
 
 export default function Dashboard() {
   const { user } = useAuth();
-  console.log(user);
+
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold">Welcome {user?.firstName}</h1>
--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -1,67 +1,63 @@
 import { create } from "zustand"
+import { persist, createJSONStorage } from "zustand/middleware"
 import type { ApiError, AuthState, User } from "../types/auth.types"
 import { loginController } from "../controllers/auth.controller"
 import { useToastStore } from "./toast.store"
 import axios from "axios"
 
-export const useAuthStore = create<AuthState>((set) => ({
-    user: null,
-    token: localStorage.getItem("token"),
-    loading: false,
 
-    setUser: (user: User | null) =>
-        set({
-            user,
-        }),
-
-    login: async (username: string, password: string) => {
-        set({ loading: true })
-
-        try {
-            const data = await loginController(username, password)
-
-            const user: User = {
-                id: data.id,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                email: data.email,
-            }
-
-
-            localStorage.setItem("token", data.accessToken)
-
-            set({
-                user,
-                token: data.accessToken,
-                loading: false,
-            })
-
-            return true
-        } catch (error) {
-            set({ loading: false })
-
-            if (axios.isAxiosError<ApiError>(error)) {
-                const message =
-                    error.response?.data?.message || "Something went wrong"
-
-                useToastStore.getState().showToast(message, "error")
-            } else {
-                useToastStore.getState().showToast(
-                    "Unexpected error occurred",
-                    "error"
-                )
-            }
-
-            return false
-        }
-    },
-
-    logout: () => {
-        localStorage.removeItem("token")
-
-        set({
+export const useAuthStore = create<AuthState>()(
+    persist(
+        (set) => ({
             user: null,
             token: null,
-        })
-    },
-}))
+            loading: false,
+
+            setUser: (user: User | null) => set({ user }),
+
+            login: async (username: string, password: string) => {
+                set({ loading: true })
+                try {
+                    const data = await loginController(username, password)
+                    const user: User = {
+                        id: data.id,
+                        firstName: data.firstName,
+                        lastName: data.lastName,
+                        email: data.email,
+                    }
+                    set({
+                        user,
+                        token: data.accessToken,
+                        loading: false,
+                    })
+                    return true
+                } catch (error) {
+                    set({ loading: false })
+                    if (axios.isAxiosError<ApiError>(error)) {
+                        const message = error.response?.data?.message || "Something went wrong"
+                        useToastStore.getState().showToast(message, "error")
+                    } else {
+                        useToastStore.getState().showToast("Unexpected error occurred", "error")
+                    }
+                    return false
+                }
+            },
+
+            logout: () => {
+
+                set({
+                    user: null,
+                    token: null,
+                })
+            },
+        }),
+        {
+            name: "auth-storage",
+            storage: createJSONStorage(() => localStorage),
+            partialize: (state) => ({
+                user: state.user,
+                token: state.token
+            }),
+        }
+    )
+)
